90ccee1a8c66c063cc922dd0cd6946407f26e7eb
Bart van Helvert <B.A.J.v.Helvert@student.tue.nl>
2019-11-04 21:03:01 +0100

    Add automatic licensing

--- src/main/kotlin/io/guthix/oldscape/server/ServerConfig.kt
+++ src/main/kotlin/io/guthix/oldscape/server/ServerConfig.kt
@@ -1,7 +1,7 @@
-/*
+/**
  * This file is part of Guthix OldScape.
  *
- * Foobar is free software: you can redistribute it and/or modify
+ * Guthix OldScape is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
@@ -12,7 +12,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Guthix OldScape. If not, see <https://www.gnu.org/licenses/>.
+ * along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
  */
 package io.guthix.oldscape.server
 
