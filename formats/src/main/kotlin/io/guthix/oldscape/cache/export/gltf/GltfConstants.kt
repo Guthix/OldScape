fda19aabf39cd75a1b0258a33305a350badb9570
Bart van Helvert <B.A.J.v.Helvert@student.tue.nl>
2019-11-04 21:03:01 +0100

    [cache] Add automatic licensing

--- formats/src/main/kotlin/io/guthix/oldscape/cache/export/gltf/GltfConstants.kt
+++ formats/src/main/kotlin/io/guthix/oldscape/cache/export/gltf/GltfConstants.kt
@@ -1,19 +1,18 @@
-/*
- * Copyright (C) 2019 Guthix
+/**
+ * This file is part of Guthix OldScape.
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 3 of the License, or (at your option) any later version.
+ * Guthix OldScape is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * Guthix OldScape is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with this program; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
  */
 package io.guthix.oldscape.cache.export.gltf
 
