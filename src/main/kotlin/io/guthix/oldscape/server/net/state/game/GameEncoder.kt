ebe7189fba97fe991491cf89e9f9ae3e983267d7
Bart van Helvert <B.A.J.v.Helvert@student.tue.nl>
2019-11-07 15:31:51 +0100

    [server] Change license spacing in files

--- src/main/kotlin/io/guthix/oldscape/server/net/state/game/GameEncoder.kt
+++ src/main/kotlin/io/guthix/oldscape/server/net/state/game/GameEncoder.kt
@@ -8,15 +8,14 @@
  *
  * Guthix OldScape is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+ * along with Foobar. If not, see <https://www.gnu.org/licenses/>.
  */
 package io.guthix.oldscape.server.net.state.game
 
-import io.guthix.oldscape.server.GameEvent
 import io.guthix.oldscape.server.net.state.IsaacRandom
 import io.netty.buffer.ByteBuf
 import io.netty.channel.ChannelHandlerContext
