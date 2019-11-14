b61b937f96546daa78b9c1e6566ae3c01d942c05
Bart van Helvert <B.A.J.v.Helvert@student.tue.nl>
2019-11-14 18:21:01 +0100

    Replace example event by login event

--- scripts/src/main/kotlin/io/guthix/oldscape/server/ExampleScript.kts
+++
@@ -1,7 +0,0 @@
-import io.guthix.oldscape.server.event.ExampleEvent
-
-on(ExampleEvent::class).where { testValue == 3 }.then {
-    println(testValue)
-    println(world.isFull)
-    println(player.index)
-}
