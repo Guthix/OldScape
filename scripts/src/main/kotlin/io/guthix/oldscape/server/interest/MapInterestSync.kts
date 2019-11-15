10e639960b7e8d1cee72d9ac725f6adf670246fb
Bart van Helvert <B.A.J.v.Helvert@student.tue.nl>
2019-11-15 18:07:56 +0100

    Add minimal and maximal zone fetching

--- scripts/src/main/kotlin/io/guthix/oldscape/server/interest/MapInterestSync.kts
+++ scripts/src/main/kotlin/io/guthix/oldscape/server/interest/MapInterestSync.kts
@@ -1,13 +1,18 @@
 import io.guthix.oldscape.server.event.imp.LoginEvent
 import io.guthix.oldscape.server.interest.InterestConstants.ZONE_INTEREST_UPDATE
+import io.guthix.oldscape.server.interest.InterestConstants.ZONE_INTEREST_RANGE
+import io.guthix.oldscape.server.world.mapsquare.zone.ZoneDim
 
 on(LoginEvent::class).then {
     var lastUpdatedZone = player.position.zone
     while(true) {
         if(lastUpdatedZone.withInDistanceOf(player.position.zone, ZONE_INTEREST_UPDATE)) continue
-        val mapSquare = player.position.zone.mapSquare
-
+        val curZone = player.position.zone
         lastUpdatedZone = player.position.zone
         wait(ticks = 1)
     }
 }
+
+fun endMapSquare(zone: ZoneDim) = (zone + ZONE_INTEREST_RANGE).mapSquare
+
+fun startMapSquare(zone: ZoneDim) = (zone - ZONE_INTEREST_RANGE).mapSquare
