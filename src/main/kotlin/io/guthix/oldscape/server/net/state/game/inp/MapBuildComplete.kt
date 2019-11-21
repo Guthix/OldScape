577f77deeb42be42a6feb2529954189e622673fb
Bart van Helvert <B.A.J.v.Helvert@student.tue.nl>
2019-11-21 17:21:27 +0100

    Add map build complete packet

---
+++ src/main/kotlin/io/guthix/oldscape/server/net/state/game/inp/MapBuildComplete.kt
@@ -0,0 +1,29 @@
+/**
+ * This file is part of Guthix OldScape.
+ *
+ * Guthix OldScape is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Guthix OldScape is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar. If not, see <https://www.gnu.org/licenses/>.
+ */
+package io.guthix.oldscape.server.net.state.game.inp
+
+import io.guthix.oldscape.server.event.GameEvent
+import io.guthix.oldscape.server.net.state.game.FixedSize
+import io.guthix.oldscape.server.net.state.game.GamePacketDecoder
+import io.netty.buffer.ByteBuf
+import io.netty.channel.ChannelHandlerContext
+
+object MapBuildComplete : GameEvent
+
+class MapBuildCompleteDecoder : GamePacketDecoder(49, FixedSize(0)) {
+    override fun decode(data: ByteBuf, ctx: ChannelHandlerContext) = MapBuildComplete
+}
