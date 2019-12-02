323c706275072075ecb1e14febf7fdafa4f06a48
Bart van Helvert <B.A.J.v.Helvert@student.tue.nl>
2019-12-02 18:13:16 +0100

    [wiki] Add item downloader

---
+++ toolbox/src/main/kotlin/io/guthix/oldscape/wiki/JsonItemDownloader.kt
@@ -0,0 +1,78 @@
+/**
+ * This file is part of Guthix OldScape.
+ *
+ * Guthix OldScape is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Guthix OldScape is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Foobar. If not, see <https://www.gnu.org/licenses/>.
+ */
+package io.guthix.oldscape.wiki
+
+import io.guthix.cache.js5.Js5Cache
+import io.guthix.cache.js5.container.disk.Js5DiskStore
+import io.guthix.oldscape.cache.ConfigArchive
+import io.guthix.oldscape.cache.config.ItemConfig
+import io.guthix.oldscape.cache.config.NpcConfig
+import io.guthix.oldscape.wiki.wikitext.ItemWikiDefinition
+import io.guthix.oldscape.wiki.wikitext.NpcWikiDefinition
+import io.ktor.util.KtorExperimentalAPI
+import kotlinx.coroutines.*
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonConfiguration
+import kotlinx.serialization.list
+import mu.KotlinLogging
+import java.nio.file.Files
+import java.nio.file.Path
+
+private val logger = KotlinLogging.logger {}
+
+@ExperimentalCoroutinesApi
+@KtorExperimentalAPI
+fun main() {
+    val downloader = JsonItemDownloader()
+    val itemconfigs = downloader.load()
+    val wikiConfigsJson = Json(downloader.config).stringify(ItemWikiDefinition.serializer().list, itemconfigs)
+    val path = Path.of(JsonItemDownloader::class.java.getResource("/").toURI())
+    Files.writeString(path.resolve("wikiNpcConfigs.json"), wikiConfigsJson)
+}
+
+class JsonItemDownloader : JsonDownloader() {
+    @ExperimentalCoroutinesApi
+    @KtorExperimentalAPI
+    fun load() = runBlocking<List<ItemWikiDefinition>> {
+        val ds = Js5DiskStore.open(Path.of(JsonNpcDownloader::class.java.getResource("/cache").toURI()))
+        val cache = Js5Cache(ds)
+        val cacheConfigs = NpcConfig.load(cache.readArchive(ConfigArchive.id).readGroup(ItemConfig.id)).values
+        val wikiConfigs = mutableListOf<ItemWikiDefinition>()
+        val idsScraped = mutableListOf<Int>()
+        for(config in cacheConfigs) {
+            if(config.id !in idsScraped) {
+                val wikiText = requestWikiText(config.id, config.name)
+                wikiText ?: logger.error { "Could not retrieve wikitext for id ${config.id} name ${config.name}" }
+                val definitions = parseWikiString<ItemWikiDefinition>(wikiText ?: "")
+                definitions.forEach { definition ->
+                    if(definition.ids == null) {
+                        definition.ids = mutableListOf(config.id)
+                    }
+                    if(definition.name == null) {
+                        definition.name = config.name
+                    }
+                    idsScraped.addAll(definition.ids ?: mutableListOf(config.id))
+                }
+                wikiConfigs.addAll(definitions)
+            } else {
+                logger.info { "Already found wiki config for id ${config.id} name ${config.name}" }
+            }
+        }
+        wikiConfigs.sortBy { it.ids?.first() }
+        wikiConfigs
+    }
+}
