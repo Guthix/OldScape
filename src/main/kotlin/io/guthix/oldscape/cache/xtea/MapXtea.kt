c3a0c985f67f182980c7d83ae95c28f314bf3570
Bart van Helvert <B.A.J.v.Helvert@student.tue.nl>
2020-01-05 19:33:05 +0100

    [cache] Remove serialization from cache module

--- src/main/kotlin/io/guthix/oldscape/cache/xtea/MapXtea.kt
+++ src/main/kotlin/io/guthix/oldscape/cache/xtea/MapXtea.kt
@@ -16,10 +16,24 @@
  */
 package io.guthix.oldscape.cache.xtea
 
-import kotlinx.serialization.Serializable
-
-@Serializable
 data class MapXtea(val id: Int, val key: IntArray) {
     val x get() = id shr 8
+
     val y get() = id and 0xFF
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+        other as MapXtea
+        if (id != other.id) return false
+        if (!key.contentEquals(other.key)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = id
+        result = 31 * result + key.contentHashCode()
+        return result
+    }
 }
