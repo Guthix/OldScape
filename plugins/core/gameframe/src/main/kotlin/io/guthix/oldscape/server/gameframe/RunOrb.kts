a58570cec6f3d3c836c164860ed9f1783f6559fe
Bart van Helvert <B.A.J.v.Helvert@student.tue.nl>
2020-02-01 00:24:57 +0100

    Fix running

--- plugins/core/gameframe/src/main/kotlin/io/guthix/oldscape/server/gameframe/RunOrb.kts
+++ plugins/core/gameframe/src/main/kotlin/io/guthix/oldscape/server/gameframe/RunOrb.kts
@@ -4,6 +4,6 @@ import io.guthix.oldscape.server.event.imp.ButtonClickEvent
 import io.guthix.oldscape.server.net.state.game.outp.PlayerInfoPacket
 
 on(ButtonClickEvent::class).where { event.interfaceId == 160 && event.buttonId == 22 }.then {
-    player.inRunMode = true
+    player.inRunMode = !player.inRunMode
     player.updateFlags.add(PlayerInfoPacket.movementCached)
 }
