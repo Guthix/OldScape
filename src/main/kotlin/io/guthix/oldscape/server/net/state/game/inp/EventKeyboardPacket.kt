2d3cd2c0cf8845ece175e08e0d43002d1944113c
Bart van Helvert <B.A.J.v.Helvert@student.tue.nl>
2020-02-01 00:18:15 +0100

    [server] Add keyboard press recording

---
+++ src/main/kotlin/io/guthix/oldscape/server/net/state/game/inp/EventKeyboardPacket.kt
@@ -0,0 +1,39 @@
+/**
+ * This file is part of Guthix OldScape.
+ *
+ * Guthix OldScape is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Guthix OldScape is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar. If not, see <https://www.gnu.org/licenses/>.
+ */
+package io.guthix.oldscape.server.net.state.game.inp
+
+import io.guthix.oldscape.server.event.GameEvent
+import io.guthix.oldscape.server.event.imp.KeyPress
+import io.guthix.oldscape.server.event.imp.KeyboardKey
+import io.guthix.oldscape.server.event.imp.KeyboardPressEvent
+import io.guthix.oldscape.server.net.state.game.GamePacketDecoder
+import io.guthix.oldscape.server.net.state.game.VarShortSize
+import io.netty.buffer.ByteBuf
+import io.netty.channel.ChannelHandlerContext
+
+class EventKeyboardPacket : GamePacketDecoder(8, VarShortSize) {
+    override fun decode(data: ByteBuf, size: Int, ctx: ChannelHandlerContext): GameEvent {
+        val keyPresses = mutableListOf<KeyPress>()
+        for (button in 0 until (size / 4)) {
+            val keyCode = data.readUnsignedByte().toInt()
+            val timeInterval = data.readUnsignedMedium()
+            keyPresses.add(KeyPress(KeyboardKey.get(keyCode), timeInterval))
+        }
+        return KeyboardPressEvent(keyPresses.toList())
+    }
+
+}
