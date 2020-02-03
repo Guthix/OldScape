bbebfdaa1e48d1cb5a4ee1e50dabf132ee136466
Bart van Helvert <B.A.J.v.Helvert@student.tue.nl>
2020-02-03 13:41:43 +0100

    Clean up ISAAC

--- src/main/kotlin/io/guthix/oldscape/server/net/Isaac.kt
+++ src/main/kotlin/io/guthix/oldscape/server/net/Isaac.kt
@@ -20,185 +20,116 @@ class IsaacRandomPair(val encodeGen : IsaacRandom, val decodeGen: IsaacRandom)
 
 class IsaacRandom(seed: IntArray) {
     private var valuesRemaining = 0
-    private var randResult: IntArray = IntArray(256)
-    private var mm: IntArray = IntArray(256)
-    private var field2387 = 0
-    private var field2385 = 0
-    private var field2382 = 0
+    private var rsl: IntArray = IntArray(256)
+    private var mem: IntArray = IntArray(256)
+    private var isaacA = 0
+    private var isaacB = 0
+    private var isaacC = 0
 
     init {
         for (int_0 in seed.indices) {
-            randResult[int_0] = seed[int_0]
+            rsl[int_0] = seed[int_0]
         }
-        method3861()
+        initState()
     }
 
     fun nextInt(): Int {
         if (--valuesRemaining + 1 == 0) {
-            generateMoreResults()
+            isaac()
             valuesRemaining = 255
         }
-        return randResult[valuesRemaining]
+        return rsl[valuesRemaining]
     }
 
-    fun generateMoreResults() {
-        field2385 += ++field2382
-
-        for (int_0 in 0..255) {
-            val int_1 = mm[int_0]
-            field2387 = if (int_0 and 0x2 == 0) {
-                if (int_0 and 0x1 == 0) {
-                    field2387 xor (field2387 shl 13)
+    fun isaac() {
+        isaacB += ++isaacC
+        for (i in 0..255) {
+            val isaacX = mem[i]
+            isaacA = if (i and 0x2 == 0) {
+                if (i and 0x1 == 0) {
+                    isaacA xor (isaacA shl 13)
                 } else {
-                    field2387 xor field2387.ushr(6)
+                    isaacA xor isaacA.ushr(6)
                 }
-            } else if (int_0 and 0x1 == 0) {
-                field2387 xor (field2387 shl 2)
+            } else if (i and 0x1 == 0) {
+                isaacA xor (isaacA shl 2)
             } else {
-                field2387 xor field2387.ushr(16)
+                isaacA xor isaacA.ushr(16)
             }
-
-            field2387 += mm[128 + int_0 and 0xFF]
-            val int_2: Int
-            int_2 = mm[int_1 and 0x3FC shr 2] + field2385 + field2387
-            mm[int_0] = int_2
-            field2385 = mm[int_2 shr 8 and 0x3FC shr 2] + int_1
-            randResult[int_0] = field2385
+            isaacA += mem[128 + i and 0xFF]
+            mem[i] = mem[isaacX and 0x3FC shr 2] + isaacB + isaacA
+            isaacB = mem[mem[i] shr 8 and 0x3FC shr 2] + isaacX
+            rsl[i] = isaacB
         }
-
     }
 
-    private fun method3861() {
-        var int_0 = GOLDEN_RATIO
-        var int_1 = GOLDEN_RATIO
-        var int_2 = GOLDEN_RATIO
-        var int_3 = GOLDEN_RATIO
-        var int_4 = GOLDEN_RATIO
-        var int_5 = GOLDEN_RATIO
-        var int_6 = GOLDEN_RATIO
-        var int_7 = GOLDEN_RATIO
-
-        var int_8 = 0
-        while (int_8 < 4) {
-            int_7 = int_7 xor (int_6 shl 11)
-            int_4 += int_7
-            int_6 += int_5
-            int_6 = int_6 xor int_5.ushr(2)
-            int_3 += int_6
-            int_5 += int_4
-            int_5 = int_5 xor (int_4 shl 8)
-            int_2 += int_5
-            int_4 += int_3
-            int_4 = int_4 xor int_3.ushr(16)
-            int_1 += int_4
-            int_3 += int_2
-            int_3 = int_3 xor (int_2 shl 10)
-            int_0 += int_3
-            int_2 += int_1
-            int_2 = int_2 xor int_1.ushr(4)
-            int_7 += int_2
-            int_1 += int_0
-            int_1 = int_1 xor (int_0 shl 8)
-            int_6 += int_1
-            int_0 += int_7
-            int_0 = int_0 xor int_7.ushr(9)
-            int_5 += int_0
-            int_7 += int_6
-            int_8++
+    private fun initState() {
+        val arr = IntArray(8) { GOLDEN_RATIO }
+        for(i in 0 until 4) shuffle(arr)
+        for(i in 0 until 256 step 8) {
+            arr[7] += rsl[i]
+            arr[6] += rsl[i + 1]
+            arr[5] += rsl[i + 2]
+            arr[4] += rsl[i + 3]
+            arr[3] += rsl[i + 4]
+            arr[2] += rsl[i + 5]
+            arr[1] += rsl[i + 6]
+            arr[0] += rsl[i + 7]
+            shuffle(arr)
+            setState(i, arr)
         }
-
-        int_8 = 0
-        while (int_8 < 256) {
-            int_7 += randResult[int_8]
-            int_6 += randResult[int_8 + 1]
-            int_5 += randResult[int_8 + 2]
-            int_4 += randResult[int_8 + 3]
-            int_3 += randResult[int_8 + 4]
-            int_2 += randResult[int_8 + 5]
-            int_1 += randResult[int_8 + 6]
-            int_0 += randResult[int_8 + 7]
-            int_7 = int_7 xor (int_6 shl 11)
-            int_4 += int_7
-            int_6 += int_5
-            int_6 = int_6 xor int_5.ushr(2)
-            int_3 += int_6
-            int_5 += int_4
-            int_5 = int_5 xor (int_4 shl 8)
-            int_2 += int_5
-            int_4 += int_3
-            int_4 = int_4 xor int_3.ushr(16)
-            int_1 += int_4
-            int_3 += int_2
-            int_3 = int_3 xor (int_2 shl 10)
-            int_0 += int_3
-            int_2 += int_1
-            int_2 = int_2 xor int_1.ushr(4)
-            int_7 += int_2
-            int_1 += int_0
-            int_1 = int_1 xor (int_0 shl 8)
-            int_6 += int_1
-            int_0 += int_7
-            int_0 = int_0 xor int_7.ushr(9)
-            int_5 += int_0
-            int_7 += int_6
-            mm[int_8] = int_7
-            mm[int_8 + 1] = int_6
-            mm[int_8 + 2] = int_5
-            mm[int_8 + 3] = int_4
-            mm[int_8 + 4] = int_3
-            mm[int_8 + 5] = int_2
-            mm[int_8 + 6] = int_1
-            mm[int_8 + 7] = int_0
-            int_8 += 8
+        for(i in 0 until 256 step 8) {
+            arr[7] += mem[i]
+            arr[6] += mem[i + 1]
+            arr[5] += mem[i + 2]
+            arr[4] += mem[i + 3]
+            arr[3] += mem[i + 4]
+            arr[2] += mem[i + 5]
+            arr[1] += mem[i + 6]
+            arr[0] += mem[i + 7]
+            shuffle(arr)
+            setState(i, arr)
         }
-
-        int_8 = 0
-        while (int_8 < 256) {
-            int_7 += mm[int_8]
-            int_6 += mm[int_8 + 1]
-            int_5 += mm[int_8 + 2]
-            int_4 += mm[int_8 + 3]
-            int_3 += mm[int_8 + 4]
-            int_2 += mm[int_8 + 5]
-            int_1 += mm[int_8 + 6]
-            int_0 += mm[int_8 + 7]
-            int_7 = int_7 xor (int_6 shl 11)
-            int_4 += int_7
-            int_6 += int_5
-            int_6 = int_6 xor int_5.ushr(2)
-            int_3 += int_6
-            int_5 += int_4
-            int_5 = int_5 xor (int_4 shl 8)
-            int_2 += int_5
-            int_4 += int_3
-            int_4 = int_4 xor int_3.ushr(16)
-            int_1 += int_4
-            int_3 += int_2
-            int_3 = int_3 xor (int_2 shl 10)
-            int_0 += int_3
-            int_2 += int_1
-            int_2 = int_2 xor int_1.ushr(4)
-            int_7 += int_2
-            int_1 += int_0
-            int_1 = int_1 xor (int_0 shl 8)
-            int_6 += int_1
-            int_0 += int_7
-            int_0 = int_0 xor int_7.ushr(9)
-            int_5 += int_0
-            int_7 += int_6
-            mm[int_8] = int_7
-            mm[int_8 + 1] = int_6
-            mm[int_8 + 2] = int_5
-            mm[int_8 + 3] = int_4
-            mm[int_8 + 4] = int_3
-            mm[int_8 + 5] = int_2
-            mm[int_8 + 6] = int_1
-            mm[int_8 + 7] = int_0
-            int_8 += 8
-        }
-
-        generateMoreResults()
+        isaac()
         valuesRemaining = 256
+    }
+
+    private fun shuffle(arr: IntArray) {
+        arr[7] = arr[7] xor (arr[6] shl 11)
+        arr[4] += arr[7]
+        arr[6] += arr[5]
+        arr[6] = arr[6] xor arr[5].ushr(2)
+        arr[3] += arr[6]
+        arr[5] += arr[4]
+        arr[5] = arr[5] xor (arr[4] shl 8)
+        arr[2] += arr[5]
+        arr[4] += arr[3]
+        arr[4] = arr[4] xor arr[3].ushr(16)
+        arr[1] += arr[4]
+        arr[3] += arr[2]
+        arr[3] = arr[3] xor (arr[2] shl 10)
+        arr[0] += arr[3]
+        arr[2] += arr[1]
+        arr[2] = arr[2] xor arr[1].ushr(4)
+        arr[7] += arr[2]
+        arr[1] += arr[0]
+        arr[1] = arr[1] xor (arr[0] shl 8)
+        arr[6] += arr[1]
+        arr[0] += arr[7]
+        arr[0] = arr[0] xor arr[7].ushr(9)
+        arr[5] += arr[0]
+        arr[7] += arr[6]
+    }
+
+    private fun setState(start: Int, arr: IntArray) {
+        mem[start] = arr[7]
+        mem[start + 1] = arr[6]
+        mem[start + 2] = arr[5]
+        mem[start + 3] = arr[4]
+        mem[start + 4] = arr[3]
+        mem[start + 5] = arr[2]
+        mem[start + 6] = arr[1]
+        mem[start + 7] = arr[0]
     }
 
     companion object {
