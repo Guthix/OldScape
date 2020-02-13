e700d36f63c834b1869070d55d7aad22149d503b
Bart van Helvert <B.A.J.v.Helvert@student.tue.nl>
2020-02-13 12:44:17 +0100

    Add Huffman encoding

---
+++ src/main/kotlin/io/guthix/oldscape/server/api/Huffman.kt
@@ -0,0 +1,37 @@
+/**
+ * This file is part of Guthix OldScape.
+ *
+ * Guthix OldScape is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Guthix OldScape is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar. If not, see <https://www.gnu.org/licenses/>.
+ */
+package io.guthix.oldscape.server.api
+
+import io.guthix.cache.js5.Js5Archive
+import io.guthix.oldscape.cache.BinariesArchive
+import io.guthix.oldscape.cache.binary.Huffman
+import mu.KotlinLogging
+
+private val logger = KotlinLogging.logger {  }
+
+object Huffman {
+    private lateinit var huffman: Huffman
+
+    fun compress(text: String) = huffman.compress(text)
+
+    fun decompress(comp: ByteArray, length: Int) = huffman.decompress(comp, length)
+
+    fun load(archive: Js5Archive) {
+        huffman = BinariesArchive.load(archive).huffman
+        logger.info { "Loaded huffman" }
+    }
+}
