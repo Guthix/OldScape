6c4ba11d51b741138bea4928b3f61b66e8e4d888
Bart van Helvert <B.A.J.v.Helvert@student.tue.nl>
2020-03-02 00:56:12 +0100

    Clean up

--- plugins/core/pathing/src/main/kotlin/io/guthix/oldscape/server/pathing/PlayerFollow.kts
+++ plugins/core/pathing/src/main/kotlin/io/guthix/oldscape/server/pathing/PlayerFollow.kts
@@ -9,21 +9,22 @@ import io.guthix.oldscape.server.routine.NormalAction
 
 on(PlayerClickEvent::class).where { player.contextMenu[event.option - 1] == "Follow" }.then {
     val followed = world.players[event.playerIndex] ?: throw IllegalStateException()
-    val dest = DestinationTile(followed.followPosition)
+    var dest = DestinationTile(followed.followPosition)
     player.path = breadthFirstSearch(player.position, dest, player.size, true, world.map)
     player.turnToLock(followed)
     player.addRoutine(NormalAction) {
         var currentTarget = player.followPosition
         while(true) {
             if(dest.reached(player.position.x, player.position.y, player.size)) break
-            if(currentTarget != player.followPosition) { // follower moved
+            if(currentTarget != player.followPosition) {
                 player.path = breadthFirstSearch(player.position, dest, player.size, true, world.map)
             }
             wait(1)
         }
         while(true) {
             wait { currentTarget != player.followPosition }
-            player.path = simplePathSearch(player.position, DestinationTile(followed.followPosition), player.size, world.map)
+            dest = DestinationTile(followed.followPosition)
+            player.path = simplePathSearch(player.position, dest, player.size, world.map)
             currentTarget = followed.followPosition
         }
     }
