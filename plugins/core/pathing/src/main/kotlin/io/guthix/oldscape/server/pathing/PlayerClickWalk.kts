6047f21a924208d20fd3fe0f2bfba710bd30c57a
Bart van Helvert <B.A.J.v.Helvert@student.tue.nl>
2020-03-02 00:54:25 +0100

    [server] Add custom following strategy

--- plugins/core/pathing/src/main/kotlin/io/guthix/oldscape/server/pathing/PlayerClickWalk.kts
+++ plugins/core/pathing/src/main/kotlin/io/guthix/oldscape/server/pathing/PlayerClickWalk.kts
@@ -21,10 +21,7 @@ on(MapClickEvent::class).then {
 
 fun Player.startWalkingToTile(floor: FloorUnit, x: TileUnit, y: TileUnit, map: WorldMap) {
     path = breadthFirstSearch(position, DestinationTile(floor, x, y), size, true, map)
-    val end = path.last()
-    if(end != Tile(floor, x, y)) {
-        setMapFlag(end.x, end.y)
-    }
+    path.lastOrNull()?.let { dest -> if(dest != Tile(floor, x, y)) setMapFlag(dest.x, dest.y) }
     turnToLock(null) // TODO make this better
     routines.remove(NormalAction)
 }
