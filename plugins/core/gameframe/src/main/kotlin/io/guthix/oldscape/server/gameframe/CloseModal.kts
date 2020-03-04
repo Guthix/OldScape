3499a398af5a58e500b5ea671385928a7046733e
Bart van Helvert <B.A.J.v.Helvert@student.tue.nl>
2020-03-04 16:46:26 +0100

    Make models more course grained

--- plugins/core/gameframe/src/main/kotlin/io/guthix/oldscape/server/gameframe/CloseModal.kts
+++ plugins/core/gameframe/src/main/kotlin/io/guthix/oldscape/server/gameframe/CloseModal.kts
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with Foobar. If not, see <https://www.gnu.org/licenses/>.
  */
-package io.guthix.oldscape.server.options
+package io.guthix.oldscape.server.gameframe
 
 import io.guthix.oldscape.server.event.imp.CloseModalEvent
 
