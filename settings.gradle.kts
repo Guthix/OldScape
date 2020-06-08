8e9db8a5657ab9a074833b2e1bf0948da8ee32c5
Bart van Helvert <b.a.j.v.helvert@student.tue.nl>
2020-06-09 00:08:39 +0200

    [server] Improve build code

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,14 @@ import java.nio.file.Path
 import groovy.util.FileNameFinder
 
 pluginManagement {
+    val kotlinVersion by extra("1.4-M2")
+    val licensePluginVersion by extra("0.15.0")
+
+    plugins {
+        id("org.jetbrains.kotlin.jvm") version kotlinVersion
+        id("com.github.hierynomus.license") version licensePluginVersion
+    }
+
     repositories {
         maven("https://dl.bintray.com/kotlin/kotlin-eap")
         mavenCentral()
@@ -17,7 +25,7 @@ include("blueprints")
 includeModules("plugins")
 
 fun includeModules(module: String) {
-    val pluginRootDir: Path = rootProject.projectDir.toPath().resolve("plugins")
+    val pluginRootDir: Path = rootProject.projectDir.toPath().resolve(module)
     if (pluginRootDir.toFile().exists()) {
         val gradleBuildFiles = FileNameFinder().getFileNames(pluginRootDir.toString(), "**/*.gradle.kts")
         gradleBuildFiles.forEach { filename ->
