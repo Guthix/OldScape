e21c84fed8625a18452148b2838d26da9b1ade39
Bart van Helvert <b.a.j.v.helvert@student.tue.nl>
2020-06-09 18:24:20 +0200

    Remove properties file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 @file:Suppress("ConvertLambdaToReference")
 
+import org.jetbrains.kotlin.gradle.plugin.getKotlinPluginVersion
+
 plugins {
     idea
     application
@@ -11,19 +13,19 @@ group = "io.guthix"
 version = "0.1-SNAPSHOT"
 description = "An Oldschool Runescape Server Emulator"
 
-val licenseHeader by extra(file("AGPLv3.txt"))
+val licenseHeader: File by extra(file("AGPLv3.txt"))
 
 application { mainClass.set("io.guthix.oldscape.server.OldScape") }
 
-val kotlinLoggingVersion by extra("1.7.6")
-val kotlinCoroutinesVersion by extra("1.3.2")
-val classGraphVersion by extra("4.8.53")
-val logbackVersion by extra("1.2.3")
-val nettyVersion by extra("4.1.42.Final")
-val jacksonVersion by extra("2.10.2")
-val oldscapeCacheVersion by extra("1a531f49a8")
-val jagexByteBufVersion by extra("555807fda4")
-val kotlinVersion: String by extra
+val kotlinLoggingVersion: String by extra("1.7.6")
+val kotlinCoroutinesVersion: String by extra("1.3.2")
+val classGraphVersion: String by extra("4.8.53")
+val logbackVersion: String by extra("1.2.3")
+val nettyVersion: String by extra("4.1.42.Final")
+val jacksonVersion: String by extra("2.10.2")
+val oldscapeCacheVersion: String by extra("1a531f49a8")
+val jagexByteBufVersion: String by extra("555807fda4")
+val kotlinVersion: String by extra(project.getKotlinPluginVersion()!!)
 
 allprojects {
     apply(plugin = "kotlin")
@@ -72,15 +74,17 @@ dependencies {
     api(project(":dimensions"))
     api(project(":blueprints"))
     api(group = "com.github.guthix", name = "oldscape-cache", version = oldscapeCacheVersion)
-    implementation(group = "com.github.guthix", name = "jagex-byteBuf", version =  jagexByteBufVersion)
-    implementation(group = "org.jetbrains.kotlin", name = "kotlin-reflect", version =  kotlinVersion)
-    implementation(group = "org.jetbrains.kotlin", name = "kotlin-scripting-common", version =  kotlinVersion)
-    implementation(group = "org.jetbrains.kotlinx", name = "kotlinx-coroutines-core", version =  kotlinCoroutinesVersion)
+    implementation(group = "com.github.guthix", name = "jagex-byteBuf", version = jagexByteBufVersion)
+    implementation(group = "org.jetbrains.kotlin", name = "kotlin-reflect", version = kotlinVersion)
+    implementation(group = "org.jetbrains.kotlin", name = "kotlin-scripting-common", version = kotlinVersion)
+    implementation(group = "org.jetbrains.kotlinx", name = "kotlinx-coroutines-core", version = kotlinCoroutinesVersion)
     implementation(group = "io.netty", name = "netty-all", version =  nettyVersion)
     implementation(group = "io.github.classgraph", name = "classgraph", version =  classGraphVersion)
     implementation(group = "io.github.microutils", name = "kotlin-logging", version =  kotlinLoggingVersion)
     implementation(group = "ch.qos.logback", name = "logback-classic", version = logbackVersion)
     implementation(group = "com.fasterxml.jackson.core", name = "jackson-databind", version = jacksonVersion)
-    implementation(group = "com.fasterxml.jackson.dataformat", name = "jackson-dataformat-yaml", version = jacksonVersion)
+    implementation(
+        group = "com.fasterxml.jackson.dataformat", name = "jackson-dataformat-yaml", version = jacksonVersion
+    )
     implementation(group = "com.fasterxml.jackson.module", name = "jackson-module-kotlin", version = jacksonVersion)
 }
