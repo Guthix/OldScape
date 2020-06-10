95b227a1e151ad019cce6c65dfefb6541df91416
Bart van Helvert <b.a.j.v.helvert@student.tue.nl>
2020-06-10 19:25:40 +0200

    [server] Prepare repository for Jitpack publish

--- blueprints/build.gradle.kts
+++ blueprints/build.gradle.kts
@@ -1,6 +1,38 @@
+version = rootProject.version
+description = "Server Blueprints"
+
 val oldscapeCacheVersion: String by rootProject.extra
 
 dependencies {
     implementation(project(":dimensions"))
     api(group = "com.github.guthix", name = "oldscape-cache", version = oldscapeCacheVersion)
+}
+
+val dokkaJar: Jar by tasks.creating(Jar::class) {
+    group = JavaBasePlugin.DOCUMENTATION_GROUP
+    description = "Assembles Kotlin docs with Dokka"
+    classifier = "javadoc"
+    from(tasks.dokka)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("default") {
+            from(components["java"])
+            artifact(dokkaJar)
+            pom {
+                url.set("https://github.com/guthix/OldScape-Server")
+                licenses {
+                    license {
+                        name.set("GNU Lesser General Public License v3.0")
+                        url.set("https://www.gnu.org/licenses/lgpl-3.0.txt")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/guthix/OldScape-Server.git")
+                    developerConnection.set("scm:git:ssh://github.com/guthix/OldScape-Server.git")
+                }
+            }
+        }
+    }
 }
---
+++ dimensions/build.gradle.kts
@@ -0,0 +1,31 @@
+version = rootProject.version
+description = "Server Dimensions"
+
+val dokkaJar: Jar by tasks.creating(Jar::class) {
+    group = JavaBasePlugin.DOCUMENTATION_GROUP
+    description = "Assembles Kotlin docs with Dokka"
+    classifier = "javadoc"
+    from(tasks.dokka)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("default") {
+            from(components["java"])
+            artifact(dokkaJar)
+            pom {
+                url.set("https://github.com/guthix/OldScape-Server")
+                licenses {
+                    license {
+                        name.set("GNU Lesser General Public License v3.0")
+                        url.set("https://www.gnu.org/licenses/lgpl-3.0.txt")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/guthix/OldScape-Server.git")
+                    developerConnection.set("scm:git:ssh://github.com/guthix/OldScape-Server.git")
+                }
+            }
+        }
+    }
+}
