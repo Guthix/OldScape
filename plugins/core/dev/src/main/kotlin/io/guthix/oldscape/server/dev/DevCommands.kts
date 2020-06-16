e47483f229f4dd2eb71503e30be1eb4fafecbb04
Bart van Helvert <b.a.j.v.helvert@student.tue.nl>
2020-06-16 17:04:36 +0200

    Update license header

--- plugins/core/dev/src/main/kotlin/io/guthix/oldscape/server/dev/DevCommands.kts
+++ plugins/core/dev/src/main/kotlin/io/guthix/oldscape/server/dev/DevCommands.kts
@@ -1,12 +1,12 @@
 /**
- * This file is part of Guthix OldScape.
+ * This file is part of Guthix OldScape-Server.
  *
- * Guthix OldScape is free software: you can redistribute it and/or modify
+ * Guthix OldScape-Server is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Guthix OldScape is distributed in the hope that it will be useful,
+ * Guthix OldScape-Server is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Affero General Public License for more details.
@@ -70,10 +70,6 @@ on(ClientCheatEvent::class).where { event.string == "clear" }.then {
 
 on(ClientCheatEvent::class).where { event.string == "pos" }.then {
     println("Position ${player.pos}")
-}
-
-on(ClientCheatEvent::class).where { event.string == "npc" }.then {
-    world.addNpc(42, player.pos.copy(x = player.pos.x + 2.tiles))
 }
 
 on(ClientCheatEvent::class).where { event.string == "npc" }.then {
