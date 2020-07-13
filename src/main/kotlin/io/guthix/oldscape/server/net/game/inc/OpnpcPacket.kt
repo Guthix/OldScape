74ecab49084f79bb144978fa7c4d48f6b2411c71
Bart van Helvert <b.a.j.v.helvert@student.tue.nl>
2020-07-13 11:27:56 +0200

    [server] Fix copyright notice

--- src/main/kotlin/io/guthix/oldscape/server/net/game/inc/OpnpcPacket.kt
+++ src/main/kotlin/io/guthix/oldscape/server/net/game/inc/OpnpcPacket.kt
@@ -12,24 +12,9 @@
  * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with Foobar. If not, see <https://www.gnu.org/licenses/>.
+ * along with Guthix OldScape-Server. If not, see <https://www.gnu.org/licenses/>.
  */
-/*
- * This file is part of Guthix OldScape-Server.
- *
- * Guthix OldScape-Server is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Guthix OldScape-Server is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with Foobar. If not, see <https://www.gnu.org/licenses/>.
- */
+
 package io.guthix.oldscape.server.net.game.inc
 
 import io.guthix.buffer.*
--- src/main/kotlin/io/guthix/oldscape/server/net/js5/Js5Decoder.kt
+++ src/main/kotlin/io/guthix/oldscape/server/net/js5/Js5Decoder.kt
@@ -12,7 +12,7 @@
  * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with Foobar. If not, see <https://www.gnu.org/licenses/>.
+ * along with Guthix OldScape-Server. If not, see <https://www.gnu.org/licenses/>.
  */
 package io.guthix.oldscape.server.net.js5
 
@@ -28,7 +28,11 @@ class Js5Decoder : ByteToMessageDecoder() {
             Js5Type.NORMAL_CONTAINER_REQUEST.opcode, Js5Type.URGENT_CONTAINER_REQUEST.opcode -> {
                 val indexFileId = inc.readUnsignedByte().toInt()
                 val containerId = inc.readUnsignedShort()
-                out.add(Js5ContainerRequest(opcode == Js5Type.URGENT_CONTAINER_REQUEST.opcode, indexFileId, containerId))
+                out.add(Js5ContainerRequest(
+                    opcode == Js5Type.URGENT_CONTAINER_REQUEST.opcode,
+                    indexFileId,
+                    containerId
+                ))
             }
             Js5Type.CLIENT_LOGGED_IN.opcode, Js5Type.CLIENT_LOGGED_OUT.opcode -> {
                 val statusCode = inc.readUnsignedMedium()
