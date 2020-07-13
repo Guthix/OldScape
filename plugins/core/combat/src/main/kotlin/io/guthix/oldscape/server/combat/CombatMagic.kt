319a477d78df38ba8f4f83456e881667f8d05cb9
Bart van Helvert <b.a.j.v.helvert@student.tue.nl>
2020-07-13 10:03:32 +0200

    [server] Add combat spells

---
+++ plugins/core/combat/src/main/kotlin/io/guthix/oldscape/server/combat/CombatMagic.kt
@@ -0,0 +1,33 @@
+/*
+ * This file is part of Guthix OldScape-Server.
+ *
+ * Guthix OldScape-Server is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Guthix OldScape-Server is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Foobar. If not, see <https://www.gnu.org/licenses/>.
+ */
+package io.guthix.oldscape.server.combat
+
+import io.guthix.oldscape.server.world.entity.CharacterProperty
+import io.guthix.oldscape.server.world.entity.Player
+
+class CombatSpell(
+    val spotAnimationId: Int,
+    val startHeight: Int,
+    val targetHeight: Int,
+    val speed: Int,
+    val speedDelay: Int,
+    val delay: Int,
+    val angle: Int,
+    val steepness: Int
+)
+
+var Player.selectedSpell: CombatSpell by CharacterProperty()
