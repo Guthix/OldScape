027fa99df3f7223dcb27bd171cc748b9b9cba114
Bart van Helvert <bart.vanhelvert@jetbrains.com>
2021-12-04 14:49:56 +0100

    Auto download cache

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,11 @@
 @file:Suppress("ConvertLambdaToReference")
 
+import de.undercouch.gradle.tasks.download.Download
+
 plugins {
     kotlin("jvm") version "1.5.30"
     id("com.github.gmazzo.buildconfig") version "2.0.2"
+    id("de.undercouch.download") version "4.1.2"
 }
 
 buildConfig {
@@ -13,6 +16,30 @@ buildConfig {
         value = "Path.of(\"${project(":cache").projectDir.resolve("src/main/resources")}\")"
             .replace("\\", "/")
     )
+}
+
+tasks { // download OpenRS2 archive
+    val openRS2Id = 139 // cache id
+    val downloadCache by registering(Download::class) {
+        src("https://archive.openrs2.org/caches/$openRS2Id/disk.zip")
+        dest(File(buildDir, "Osrs189.zip"))
+        overwrite(false)
+    }
+    val unzipCache by registering(Copy::class) {
+        dependsOn(downloadCache)
+        from(zipTree(downloadCache.get().dest))
+        into(buildDir)
+    }
+    val downloadXteas by registering(Download::class) {
+        dependsOn(unzipCache)
+        src("https://archive.openrs2.org/caches/$openRS2Id/keys.json")
+        dest(File(buildDir, "cache/xteas.json"))
+        overwrite(false)
+    }
+    getByName("processResources").dependsOn(unzipCache)
+    getByName("processResources").dependsOn(downloadXteas)
+    getByName("compileKotlin").dependsOn(unzipCache)
+    getByName("compileKotlin").dependsOn(downloadXteas)
 }
 
 allprojects {
